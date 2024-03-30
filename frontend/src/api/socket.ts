f4d8883429549becc6c8cd98fb3ec48b2d06f48d
Maxwell Kopitz <mlkopitz@umich.edu>
2024-03-29 23:30:16 -0400

    wip

--- frontend/src/api/socket.ts
+++ frontend/src/api/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
-import { API_URL } from '../config'
+import { SOCKET_URL } from '../config'
 
-export const socket = io(API_URL,
+export const socket = io(SOCKET_URL,
     {
         withCredentials: true,
         autoConnect: false
