3fad56672c6bcbe86184c9f699aaf5499cdeb97c
Maxwell Kopitz <mlkopitz@umich.edu>
2024-04-02 20:32:47 -0400

    wip: socket rewrite

--- frontend/src/api/socket.ts
+++ frontend/src/api/socket.ts
@@ -1,11 +1,11 @@
-import { io } from 'socket.io-client'
+import { Manager } from 'socket.io-client'
 import { SOCKET_URL } from '../config'
 
-export const socket = io(SOCKET_URL,
-    {
-        withCredentials: true,
-        autoConnect: false
-    })
+export const manager = new Manager(SOCKET_URL, {
+    autoConnect: false,
+    withCredentials: true,
+});
+export const socket = manager.socket('/')
 
 export const SocketEvents = {
     GAME_CREATED: 'game-created', // recieved
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const ENV = process.env.NODE_ENV || 'development';
 export const API_URL: any = ENV === 'production' ? undefined : 'http://localhost:4000/api/v1'
-export const SOCKET_URL: any = ENV === 'production' ? undefined : 'http://localhost:4000/'
+export const SOCKET_URL: any = ENV === 'production' ? undefined : 'ws://localhost:4000/'
 
